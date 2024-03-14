a6f61ad83da74d1798fe8a310023b9605ef73d01
Rubén del Rey <ruben.delrey@babelgroup.com>
2024-03-14 13:17:11 +0100

    Implementación del servicio y el repositorio para las cartas

---
+++ src/main/java/com/babelgroup/repository/menu/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.babelgroup.repository.menu;
+
+import com.babelgroup.model.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, String> {
+}
---
+++ src/main/java/com/babelgroup/service/menu/MenuServiceImpl.java
@@ -0,0 +1,58 @@
+package com.babelgroup.service.menu;
+
+import com.babelgroup.dtos.MenuDto;
+import com.babelgroup.model.Menu;
+import com.babelgroup.model.Product;
+import com.babelgroup.model.relations.ProductMenu;
+import com.babelgroup.repository.ProductRepository;
+import com.babelgroup.repository.menu.MenuRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuServiceImpl implements MenuService {
+
+    private final MenuRepository menuRepository;
+    private final ProductRepository productRepository;
+
+    public MenuServiceImpl(MenuRepository menuRepository, ProductRepository productRepository) {
+        this.menuRepository = menuRepository;
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void add(MenuDto dto) {
+        Menu menu = new Menu();
+        menu.setName(dto.name);
+        menuRepository.save(menu);
+    }
+
+    @Override
+    public void remove(String id) {
+        menuRepository.deleteById(id);
+    }
+
+    @Override
+    public void update(MenuDto dto) {
+        Menu menu = menuRepository.findById(dto.id).orElseThrow();
+
+        if (dto.name != null){
+            menu.setName(dto.name);
+        }
+    }
+
+    @Override
+    public void addProduct(String menuId, String productId) {
+        Menu menu = menuRepository.findById(menuId).orElseThrow();
+        Product product = productRepository.findById(productId).orElseThrow();
+
+        ProductMenu.link(product, menu);
+    }
+
+    @Override
+    public void removeProduct(String menuId, String productId) {
+        Menu menu = menuRepository.findById(menuId).orElseThrow();
+        Product product = productRepository.findById(productId).orElseThrow();
+
+        ProductMenu.unlink(product, menu);
+    }
+}
